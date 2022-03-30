ec3c1ee80ff148c0db7c758d3da2abc67cbe19c9
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-03-30 11:26:42 +0630

    blog

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2022 at 09:46 AM
+-- Generation Time: Mar 28, 2022 at 03:49 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `body`, `user_id`, `date`) VALUES
+(13, 'Obcaecati molestiae ', '                Obcaecati molestiae               ', 24, '2022-03-28 19:12:46'),
+(14, 'Autem ab aliquam inc', 'Deleniti ipsum amet', 24, '2022-03-28 19:12:51'),
+(15, 'Qui est labore quo n', 'Facere sit duis unde', 24, '2022-03-28 19:27:19');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -40,12 +63,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `date`) VALUES
-(24, 'Emily', 'emily@gmail.com', 'password', '2022-03-22 12:39:09'),
-(25, 'John', 'john@gmail.com', 'johnpassword', '2022-03-22 12:47:15');
+(24, 'Emily Chris', 'emily@gmail.com', 'emily123', '2022-03-22 12:39:09'),
+(25, 'John Johnny', 'john@gmail.com', 'johnpassword', '2022-03-22 12:47:15'),
+(26, 'Katherine', 'katherine@gmail.com', 'katherine123', '2022-03-23 14:38:21');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -59,10 +89,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
