93115786b2867bf6786d4a4ef68739fa1b010be7
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-06 12:48:21 +0630

    Post Create

--- post-create.php
+++ post-create.php
@@ -1,7 +1,9 @@
 <?php
+
 include_once './init.php';
 
 include app_path('middleware/auth.php');
+
 ?>
 
 <?php include './header.php' ?>
--- profile.php
+++ profile.php
@@ -3,6 +3,7 @@ include_once './init.php';
 
 include app_path('middleware/auth.php');
 ?>
+
 <?php include './header.php' ?>
 <?php include './navbar.php' ?>
 
